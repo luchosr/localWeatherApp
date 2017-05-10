a1cd052c0c6ac60c3d6992791c2d5e51405a4ee0
Luciano Ramello <lucianoramello@MacBook-Pro-de-Luciano.local>
2017-05-10 01:01:05 -0300

    commited more visual changes, and some code features

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
       lon = position.coords.longitude;
       console.log("Latidud: " + lat);
       console.log("Longitud: " + lon);
-      $("#data").html("latitude: " + lat + "<br>longitude: " + lon);
+
 
       var api = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=550b915fae53857d3853fc8509e34f54';
       console.log("aca salimos del if");
@@ -16,13 +16,18 @@ $(document).ready(function() {
       //var api = 'http://api.openweathermap.org/data/2.5/weather?lat=-34,6&lon=-58,4&appid=550b915fae53857d3853fc8509e34f54';
 
       $.getJSON(api, function(data) {
+        // $("#icono").
+        $("#city").text(data.name);
+
         console.log(data);
         var tempC = (data.main.temp - 273,15);
-        console.log("la temperatura actual es de: " + temp);
+        $(".temp").text(tempC + "ºC");
         var tempF = tempC * 1.8 + 32;
         var city = data.name;
         console.log("En la ciudad de: " + city);
         var condiciones = data.weather[0].description;
+        console.log("Las condiciones son: " + condiciones);
+        $("#condiciones").text(condiciones);
         console.log("Las condiciones generales son: " + condiciones);
         var velViento = Math.round(data.wind.speed * 1.852 * 100)/100;
         console.log("La velocidad del viento es de. " + velViento+ " kilometros por hora.");
